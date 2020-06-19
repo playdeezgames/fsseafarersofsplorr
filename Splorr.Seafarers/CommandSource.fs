92ec245f492fbf83f4ec9de99100c69bd6de0afd
ernestpazera <ernestpazera@gmail.com>
2020-06-19 12:54:20 -0500

    finished with set header command source

--- Splorr.Seafarers/CommandSource.fs
+++ Splorr.Seafarers/CommandSource.fs
@@ -1,6 +1,7 @@
 ﻿namespace Splorr.Seafarers
 
 open Splorr.Seafarers.Views
+open Splorr.Seafarers.Models
 
 module CommandSource=
     let private ParseSetSpeed(tokens: string list) : Command option =
@@ -12,11 +13,36 @@ module CommandSource=
             | _ -> None
         | _ -> None
 
+    let private ParseDms(d:string, m: string, s:string) : Dms option =
+        match System.Int32.TryParse(d), System.Int32.TryParse(m), System.Double.TryParse(s) with
+        | (true, degrees), (true, minutes), (true, seconds) ->
+            {
+                Degrees = degrees
+                Minutes = minutes
+                Seconds = seconds
+            }
+            |> Some
+        | _ -> None
+
+    let private ParseSetHeading(tokens:string list) : Command option = 
+        (match tokens with
+        | [degrees; minutes; seconds] ->
+            ParseDms(degrees,minutes,seconds)
+        | [degrees; minutes] ->
+            ParseDms(degrees,minutes,"0.0")
+        | [degrees] ->
+            ParseDms(degrees,"0","0.0")
+        | _ -> None)
+        |> Option.map(fun x -> x |> Heading |> Set)
+
     let private ParseSet(tokens: string list) : Command option =
         match tokens with
         | "speed" :: tail ->
             tail
             |> ParseSetSpeed 
+        | "heading" :: tail ->
+            tail
+            |> ParseSetHeading
         | _ -> None
 
     let private Parse(tokens:string list) : Command option =
