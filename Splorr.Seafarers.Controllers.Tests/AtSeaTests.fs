02c040699c4212cbe4385955546ef0ffbbd54e0f
ernestpazera <ernestpazera@gmail.com>
2020-06-30 18:28:03 -0500

    some atsea test refactoring

--- Splorr.Seafarers.Controllers.Tests/AtSeaTests.fs
+++ Splorr.Seafarers.Controllers.Tests/AtSeaTests.fs
@@ -9,25 +9,45 @@ open AtSeaTestFixtures
 
 [<Test>]
 let ``Run.It returns ConfirmQuit when given Quit command.`` () =
+    let subject = world
+    let expected = 
+        subject
+        |> Gamestate.AtSea 
+        |> Gamestate.ConfirmQuit 
+        |> Some
     let actual = 
-        world
+        subject
         |> AtSea.Run random (fun ()->Some Command.Quit) sink
-    Assert.AreEqual(world |> Gamestate.AtSea |> Gamestate.ConfirmQuit |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns AtSea when given invalid command.`` () =
+    let subject = world
+    let expected = 
+        subject 
+        |> Gamestate.AtSea 
+        |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->None) sink
-    Assert.AreEqual(world |> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns AtSea with new speed when given Set Speed command.`` () =
     let newSpeed = 0.5
+    let subject = world
+    let expectedAvatar = {subject.Avatar with Speed=newSpeed}
+    let expectedMessages = ["You set your speed to 0.500000."]
+    let expected = 
+        {subject with 
+            Avatar = expectedAvatar
+            Messages= expectedMessages}
+        |> Gamestate.AtSea 
+        |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->newSpeed |> SetCommand.Speed |> Command.Set |> Some) sink
-    Assert.AreEqual({world with Avatar = { world.Avatar with Speed=newSpeed}; Messages=["You set your speed to 0.500000."] }|> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns AtSea with new heading when given Set Heading command.`` () =
@@ -37,83 +57,111 @@ let ``Run.It returns AtSea with new heading when given Set Heading command.`` ()
             Minutes = 2
             Seconds = 3.0
         }
+    let subject = world
+    let expected = 
+        {subject with Avatar = { subject.Avatar with Heading = newHeading |> Dms.ToFloat}; Messages=["You set your heading to 1\u00b02'3.000000\"."] }
+        |> Gamestate.AtSea 
+        |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->newHeading |> SetCommand.Heading |> Command.Set |> Some) sink
-    Assert.AreEqual({world with Avatar = { world.Avatar with Heading = newHeading |> Dms.ToFloat}; Messages=["You set your heading to 1\u00b02'3.000000\"."] }|> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It moves the avatar when given Move command.`` () =
+    let subject = world
+    let expected = 
+        {subject with Avatar = {subject.Avatar with Position=(6.0,5.0)}; Messages=["Steady as she goes."]; Turn=1u} 
+        |> Gamestate.AtSea 
+        |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->1u |> Command.Move |> Some) sink
-    Assert.AreEqual({world with Avatar = {world.Avatar with Position=(6.0,5.0)}; Messages=["Steady as she goes."]; Turn=1u} |> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns At Sea Help when given the Help command.`` () =
+    let subject = world
+    let expected = subject |> Gamestate.AtSea |> Gamestate.Help |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->Command.Help |> Some) sink
-    Assert.AreEqual(world |> Gamestate.AtSea |> Gamestate.Help |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 
 [<Test>]
 let ``Run.It returns Main Menu when given the Menu command.`` () =
+    let subject = world
+    let expected = subject |> Some |> Gamestate.MainMenu |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->Command.Menu |> Some) sink
-    Assert.AreEqual(world |> Some |> Gamestate.MainMenu |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 
 [<Test>]
 let ``Run.It returns Island List when given the Islands command.`` () =
+    let subject = world
+    let expected = (0u, subject |> Gamestate.AtSea) |> Gamestate.IslandList |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun()->0u |> Command.Islands |> Some) sink
-    Assert.AreEqual((0u, world |> Gamestate.AtSea) |> Gamestate.IslandList |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns AtSea when given the Dock command and there is no near enough island.`` () =
+    let subject = emptyWorld
+    let expected = {subject with Messages=["There is no place to dock."]}|>Gamestate.AtSea|>Some
     let actual =
-        emptyWorld
+        subject
         |> AtSea.Run random (fun()->Command.Dock |> Some) sink
-    Assert.AreEqual({emptyWorld with Messages=["There is no place to dock."]}|>Gamestate.AtSea|>Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
-let ``Run.It returns Docked when given the Dock command and there is a near enough island.`` () =
+let ``Run.It returns Docked (at Dock) when given the Dock command and there is a near enough island.`` () =
+    let subject = dockWorld
+    let updatedIsland = subject.Islands.[(0.0, 0.0)] |> Island.AddVisit subject.Turn
+    let expected = (Dock, (0.0,0.0),{subject with Messages = ["You dock."]; Islands = subject.Islands |> Map.add (0.0,0.0) updatedIsland })|>Gamestate.Docked|>Some
     let actual =
-        dockWorld
+        subject
         |> AtSea.Run random (fun()->Command.Dock |> Some) sink
-    let updatedIsland = dockWorld.Islands.[(0.0, 0.0)] |> Island.AddVisit dockWorld.Turn
-    Assert.AreEqual((Dock, (0.0,0.0),{dockWorld with Messages = ["You dock."]; Islands = dockWorld.Islands |> Map.add (0.0,0.0) updatedIsland })|>Gamestate.Docked|>Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It gives a message when given a Head For command and the given island does not exist.`` () =
+    let subject = headForWorldUnvisited
+    let expected = {subject with Messages=["I don't know how to get to `foo`."]} |> Gamestate.AtSea |> Some
     let actual = 
-        headForWorldUnvisited
+        subject
         |> AtSea.Run random (fun () -> "foo" |> Command.HeadFor |> Some) sink
-    Assert.AreEqual({headForWorldUnvisited with Messages=["I don't know how to get to `foo`."]} |> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It gives a message when given a Head For command and the given island exists but is not known.`` () =
+    let subject = headForWorldUnvisited
+    let expected = {subject with Messages=["I don't know how to get to `yermom`."]} |> Gamestate.AtSea |> Some
     let actual = 
-        headForWorldUnvisited
+        subject
         |> AtSea.Run random (fun () -> "yermom" |> Command.HeadFor |> Some) sink
-    Assert.AreEqual({headForWorldUnvisited with Messages=["I don't know how to get to `yermom`."]} |> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It gives a message and changes heading when given a Head For command and the given island exists and is known.`` () =
+    let subject = headForWorldVisited
+    let expected = {subject with Messages=["You set your heading to 180\u00b00'0.000000\"."; "You head for `yermom`."]; Avatar={subject.Avatar with Heading = System.Math.PI}} |> Gamestate.AtSea |> Some
     let actual = 
-        headForWorldVisited
+        subject
         |> AtSea.Run random (fun () -> "yermom" |> Command.HeadFor |> Some) sink
-    Assert.AreEqual({headForWorldVisited with Messages=["You set your heading to 180\u00b00'0.000000\"."; "You head for `yermom`."]; Avatar={headForWorldVisited.Avatar with Heading = System.Math.PI}} |> Gamestate.AtSea |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It returns Status when given the command Status.`` () =
+    let subject = world
+    let expected = subject |> Gamestate.AtSea |> Gamestate.Status |> Some
     let actual =
-        world
+        subject
         |> AtSea.Run random (fun () -> Command.Status |> Some) sink
-    Assert.AreEqual(world |> Gamestate.AtSea |> Gamestate.Status |> Some, actual)
+    Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Run.It gives a message when given the command Abandon Job and the avatar has no current job.`` () =
