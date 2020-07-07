81f57df41d8794353d20ac9326bb745fca178c7b
ernestpazera <ernestpazera@gmail.com>
2020-07-07 05:19:22 -0500

    all of the scaffolding in the ui is ready to start developing the actual persistence mechanism

---
+++ Splorr.Seafarers.Controllers.Tests/SaveGameTests.fs
@@ -0,0 +1,22 @@
+﻿module SaveGameTests
+
+open NUnit.Framework
+open Splorr.Seafarers.Services
+open Splorr.Seafarers.Controllers
+open AtSeaTestFixtures
+open CommonTestFixtures
+
+[<Test>]
+let ``Run.It returns Main Menu after saving the game.`` () =
+    let input = world
+    let inputName = "name"
+    let expected =
+        input
+        |> Some
+        |> Gamestate.MainMenu
+        |> Some
+    let actual =
+        (inputName, input)
+        ||> SaveGame.Run sinkStub
+    Assert.AreEqual(expected, actual)
+
---
+++ Splorr.Seafarers.Controllers/SaveGame.fs
@@ -0,0 +1,18 @@
+﻿namespace Splorr.Seafarers.Controllers
+
+open Splorr.Seafarers.Models
+open Splorr.Seafarers.Services
+
+module SaveGame =
+    let Run (sink:MessageSink) (name:string) (world:World) : Gamestate option =
+        [
+            "" |> Line
+            "You saved the game." |> Line
+        ]
+        |> List.iter sink
+
+        world
+        |> Some
+        |> Gamestate.MainMenu
+        |> Some
+
