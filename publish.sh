6028fbb22e7205ac89108934769362e411df66ca
ernestpazera <ernestpazera@gmail.com>
2020-07-13 21:10:22 -0500

    some more todos, and some more attempts at publishing

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-dotnet publish -c Release -o ./deploy/win ./Splorr.Seafarers/Splorr.Seafarers.fsproj
-dotnet publish -c Release -r osx-x64 -o ./deploy/mac ./Splorr.Seafarers/Splorr.Seafarers.fsproj
+dotnet publish -c Release --runtime win-x64 --framework netcoreapp3.1 -o ./deploy/win ./Splorr.Seafarers/Splorr.Seafarers.fsproj
+dotnet publish -c Release --runtime osx-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/mac ./Splorr.Seafarers/Splorr.Seafarers.fsproj
+dotnet publish -c Release --runtime linux-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/linux ./Splorr.Seafarers/Splorr.Seafarers.fsproj
+dotnet publish -c Release --runtime linux-arm64 --framework netcoreapp3.1 --self-contained true -o ./deploy/pi ./Splorr.Seafarers/Splorr.Seafarers.fsproj
