9eec7d9f9d73a18236bdb89f7a9479f7e74ddaa5
ernestpazera <ernestpazera@gmail.com>
2020-07-25 11:34:12 -0500

    a change to the publish script to 1 0 0 9 and publish removes pngs and txt files so we dont ship them

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm ./deploy/win/*.png
+rm ./deploy/win/*.txt
+
+
 dotnet publish -c Release --runtime win-x64 --framework netcoreapp3.1 -o ./deploy/win ./Splorr.Seafarers/Splorr.Seafarers.fsproj
 dotnet publish -c Release --runtime osx-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/mac ./Splorr.Seafarers/Splorr.Seafarers.fsproj
 dotnet publish -c Release --runtime linux-x64 --framework netcoreapp3.1 --self-contained true -o ./deploy/linux ./Splorr.Seafarers/Splorr.Seafarers.fsproj
