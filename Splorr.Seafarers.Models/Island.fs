d2fef82751cf5940cbfdca0d31002d3266d9b6bf
ernestpazera <ernestpazera@gmail.com>
2020-08-23 04:59:13 -0500

    added the persistence layer for island statistics and statistic templates

--- Splorr.Seafarers.Models/Island.fs
+++ Splorr.Seafarers.Models/Island.fs
@@ -1,5 +1,10 @@
 ﻿namespace Splorr.Seafarers.Models
 
+type IslandStatisticIdentifier =
+    | CareenDistance = 1
+
+type IslandStatisticTemplate = StatisticTemplate
+
 type Island =
     {
         Jobs           : Job list
---
+++ Splorr.Seafarers.Persistence/IslandStatisticTemplate.fs
@@ -0,0 +1,25 @@
+﻿namespace Splorr.Seafarers.Persistence
+
+open System.Data.SQLite
+open Splorr.Seafarers.Models
+
+module IslandStatisticTemplate =
+    let private convertor 
+            (reader : SQLiteDataReader) 
+            : IslandStatisticIdentifier * IslandStatisticTemplate =
+        (reader.GetInt32(0) |> enum<IslandStatisticIdentifier>,
+            {
+                StatisticName = reader.GetString(1)
+                MinimumValue = reader.GetDouble(2)
+                MaximumValue = reader.GetDouble(3)
+                CurrentValue = reader.GetDouble(4)
+            })
+
+    let GetList 
+            (connection : SQLiteConnection) 
+            : Result<Map<IslandStatisticIdentifier, IslandStatisticTemplate>, string> =
+        connection
+        |> Utility.GetList 
+            "SELECT [StatisticId], [StatisticName], [MinimumValue], [MaximumValue], [CurrentValue] FROM [IslandStatisticTemplates];" (fun _->()) convertor
+        |> Result.map
+            (Map.ofList)
