6a340e417216b961034e0bd9e0d8e916e132955b
ernestpazera <ernestpazera@gmail.com>
2020-10-24 07:13:19 -0500

    I added a test for GetAvatarIslandFeature

---
+++ Splorr.Seafarers.Services.Tests/WorldGetAvatarIslandFeatureTests.fs
@@ -0,0 +1,19 @@
+﻿module WorldGetAvatarIslandFeatureTests
+
+open NUnit.Framework
+open Splorr.Seafarers.Services
+open Splorr.Seafarers.Models
+open Splorr.Tests.Common
+
+[<Test>]
+let ``GetAvatarIslandFeature.It gets the avatar island feature.`` () =
+    let calledGetFeature = ref false
+    let context = Contexts.TestContext()
+    (context :> AvatarIslandFeature.GetFeatureContext).avatarIslandFeatureSource :=
+        Spies.Source(calledGetFeature, None)
+    let actual =
+        World.GetAvatarIslandFeature
+            context
+            Dummies.ValidAvatarId
+    Assert.AreEqual(None, actual)
+    Assert.IsTrue(calledGetFeature.Value)
