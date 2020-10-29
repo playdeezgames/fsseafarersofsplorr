ea9d83fbe32806c94832caec47bb1b714d953913
ernestpazera <ernestpazera@gmail.com>
2020-10-29 04:17:31 -0500

    wrote test for GetVesselSpeed

--- Splorr.Seafarers.Services.Tests/WorldGetVesselSpeedTests.fs
+++ Splorr.Seafarers.Services.Tests/WorldGetVesselSpeedTests.fs
@@ -7,11 +7,15 @@ open Splorr.Tests.Common
 
 [<Test>]
 let ``GetVesselSpeed.It sets the speed of the give avatar's vessel.`` () =
+    let calledGetVesselStatistic = ref false
     let context = Contexts.TestContext()
+    (context :> Vessel.GetStatisticContext).vesselSingleStatisticSource := 
+        Spies.Source(calledGetVesselStatistic, Some {MinimumValue = 0.0; MaximumValue=100.0; CurrentValue=50.0})
     let actual = 
         World.GetVesselSpeed
             context
             Dummies.ValidAvatarId
-    Assert.AreEqual(None, actual)
+    Assert.AreEqual(Some 50.0, actual)
+    Assert.IsTrue(calledGetVesselStatistic.Value)
 
 
