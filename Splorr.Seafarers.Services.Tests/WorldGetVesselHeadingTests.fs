6f69845831730bd7aafca28d3cd6dfe2c2c7bc83
ernestpazera <ernestpazera@gmail.com>
2020-10-29 04:16:34 -0500

    wrote a test for GetVesselHeading

--- Splorr.Seafarers.Services.Tests/WorldGetVesselHeadingTests.fs
+++ Splorr.Seafarers.Services.Tests/WorldGetVesselHeadingTests.fs
@@ -7,9 +7,13 @@ open Splorr.Tests.Common
 
 [<Test>]
 let ``GetVesselHeading.It gets the heading of the give avatar's vessel.`` () =
+    let calledGetVesselStatistic = ref false
     let context = Contexts.TestContext()
+    (context :> Vessel.GetStatisticContext).vesselSingleStatisticSource := 
+        Spies.Source(calledGetVesselStatistic, Some {MinimumValue = 0.0; MaximumValue=100.0; CurrentValue=50.0})
     let actual = 
         World.GetVesselHeading
             context
             Dummies.ValidAvatarId
-    Assert.AreEqual(None, actual)
+    Assert.AreEqual(Some 50.0, actual)
+    Assert.IsTrue(calledGetVesselStatistic.Value)
