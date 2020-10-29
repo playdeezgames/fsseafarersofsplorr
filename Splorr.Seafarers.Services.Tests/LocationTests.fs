2e790238d21e9a143660a0c94ad6688a4855c065
ernestpazera <ernestpazera@gmail.com>
2020-10-29 04:34:16 -0500

    eliminated commented out code

--- Splorr.Seafarers.Services.Tests/LocationTests.fs
+++ Splorr.Seafarers.Services.Tests/LocationTests.fs
@@ -10,14 +10,6 @@ let ``DistanceTo.It returns the distance between two given locations.`` () =
     let actual = first |> Location.DistanceTo second
     Assert.AreEqual(5.0, actual)
 
-//[<Test>]
-//let ``ScaleBy.It returns the product of a given location by a given scale.`` () =
-//    let actual = 
-//        (1.0,2.0)
-//        |> Location.ScaleBy 3.0
-//    Assert.AreEqual(3.0, actual |> fst)
-//    Assert.AreEqual(6.0, actual |> snd)
-
 [<Test>]
 let ``HeadingTo.It returns the direction in radians from a given location to another given location.`` () =
     let first = (0.0,0.0)
