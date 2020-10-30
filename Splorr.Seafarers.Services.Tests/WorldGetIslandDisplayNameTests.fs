d2690d6f6cd4224cf7befd564ca1d52bc7128214
ernestpazera <ernestpazera@gmail.com>
2020-10-30 03:19:34 -0500

    i added get nearby location and get island display name tests

--- Splorr.Seafarers.Services.Tests/WorldGetIslandDisplayNameTests.fs
+++ Splorr.Seafarers.Services.Tests/WorldGetIslandDisplayNameTests.fs
@@ -4,34 +4,53 @@ open NUnit.Framework
 open Splorr.Seafarers.Services
 open Splorr.Seafarers.Models
 open Splorr.Tests.Common
+open System
 
 [<Test>]
 let ``GetIslandDisplayName.It raises an exception when the given island doesn't exist.`` () =
+    let calledGetIslandName = ref false
     let context = Contexts.TestContext()
-    let actual = 
-        World.GetIslandDisplayName
-            context
-            Dummies.ValidAvatarId
-            Dummies.ValidIslandLocation
-    Assert.AreEqual("", actual)
+    (context :> IslandName.GetNameContext).islandSingleNameSource := Spies.Source(calledGetIslandName, None)
+    Assert.Throws<NotImplementedException>(
+        fun () ->
+            World.GetIslandDisplayName
+                context
+                Dummies.ValidAvatarId
+                Dummies.InvalidIslandLocation
+            |> ignore)
+    |> ignore
+    Assert.IsTrue(calledGetIslandName.Value)
 
 [<Test>]
 let ``GetIslandDisplayName.It returns "(unknown)" then the island exists but the avatar doesn't know about it.`` () =
+    let calledGetIslandName = ref false
+    let calledGetAvatarIslandMetric = ref false
     let context = Contexts.TestContext()
+    (context :> IslandName.GetNameContext).islandSingleNameSource := Spies.Source(calledGetIslandName, Some "Island Name")
+    (context :> AvatarIslandMetric.GetContext).avatarIslandSingleMetricSource := Spies.Source(calledGetAvatarIslandMetric, None)
     let actual = 
         World.GetIslandDisplayName
             context
             Dummies.ValidAvatarId
             Dummies.ValidIslandLocation
-    Assert.AreEqual("", actual)
+    Assert.AreEqual("(unknown)", actual)
+    Assert.IsTrue(calledGetIslandName.Value)
+    Assert.IsTrue(calledGetAvatarIslandMetric.Value)
 
 [<Test>]
 let ``GetIslandDisplayName.It returns the island name when the island exists and the avatar knows about it.`` () =
+    let calledGetIslandName = ref false
+    let calledGetAvatarIslandMetric = ref false
     let context = Contexts.TestContext()
+    (context :> IslandName.GetNameContext).islandSingleNameSource := Spies.Source(calledGetIslandName, Some "Island Name")
+    (context :> AvatarIslandMetric.GetContext).avatarIslandSingleMetricSource := Spies.Source(calledGetAvatarIslandMetric, Some 0UL)
     let actual = 
         World.GetIslandDisplayName
             context
             Dummies.ValidAvatarId
             Dummies.ValidIslandLocation
-    Assert.AreEqual("", actual)
+    Assert.AreEqual("Island Name", actual)
+    Assert.IsTrue(calledGetIslandName.Value)
+    Assert.IsTrue(calledGetAvatarIslandMetric.Value)
+
 
--- Splorr.Seafarers.Services.Tests/WorldGetNearbyLocationsTests.fs
+++ Splorr.Seafarers.Services.Tests/WorldGetNearbyLocationsTests.fs
@@ -7,13 +7,17 @@ open Splorr.Tests.Common
 
 [<Test>]
 let ``GetNearbyLocations.It returns a list of island locations within a given distance of a given location.`` () =
+    let calledGetIslandList = ref false
     let context = Contexts.TestContext()
+    (context :> Island.GetListContext).islandSource :=
+        Spies.Source(calledGetIslandList, [(0.0, 1.0);(2.0,3.0);(4.0,5.0);(6.0,7.0);(8.0,9.0);(10.0,11.0);(12.0,13.0);(14.0,15.0);(16.0,17.0);(18.0,19.0)])
     let actual = 
         World.GetNearbyLocations
             context
-            Dummies.ValidIslandLocation
+            (0.0, 1.0)
             10.0
-    Assert.AreEqual([], actual)
+    Assert.AreEqual([(0.0, 1.0);(2.0,3.0);(4.0,5.0);(6.0,7.0)], actual)
+    Assert.IsTrue(calledGetIslandList.Value)
 
 
 
